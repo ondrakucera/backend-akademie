12c950abdf8b4d072b63b1c9d40156ed1da88d18
Ondra Kučera <ondrej.kucera@centrum.cz>
2026-04-04 00:06:48 +0200

    Lecture 3: hello_world: Add Hello world JSON example

--- pro_lektora/lecture_3/hello_world/index.js
+++ pro_lektora/lecture_3/hello_world/index.js
@@ -5,4 +5,8 @@ app.get("/hello-world", (req, res) => {
 	res.send("Hello world!");
 });
 
+app.get("/hello-world-json", (req, res) => {
+	res.json({ message: "Hello world!", serverTime: new Date().toISOString() });
+});
+
 app.listen(3000);
