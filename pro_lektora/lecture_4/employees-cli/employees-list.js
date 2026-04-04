ec3fe287526439b5c3f24a5cc26b284a677b7c77
Ondra Kučera <ondrej.kucera@centrum.cz>
2026-04-04 17:27:17 +0200

    Lecture 4: employees-cli: Add employees-list.js

---
+++ pro_lektora/lecture_4/employees-cli/employees-list.js
@@ -0,0 +1,18 @@
+import client from "./db-client.js";
+
+try {
+	await client.connect();
+	const result = await client.query(`
+		SELECT employee.id, employee.first_name, employee.last_name, employee.age, department.name as department_name
+		FROM employee
+		JOIN department ON employee.department_id = department.id
+		ORDER BY employee.id
+	`);
+	result.rows.forEach((employeeRow) => {
+		console.log(
+			`${employeeRow.id}: ${employeeRow.first_name} ${employeeRow.last_name} (${employeeRow.age}), ${employeeRow.department_name}`,
+		);
+	});
+} finally {
+	await client.end();
+}
