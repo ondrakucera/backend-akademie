26ffb89208e22011dee13f975b5df62762c46d47
Ondra Kučera <ondrej.kucera@centrum.cz>
2026-04-04 17:12:03 +0200

    Lecture 4: employees-cli: Add departments-update.js

---
+++ pro_lektora/lecture_4/employees-cli/departments-update.js
@@ -0,0 +1,25 @@
+import prompt from "prompt-sync";
+import client from "./db-client.js";
+
+const promptSync = prompt();
+
+try {
+	const id = promptSync("Zadej ID oddělení: ");
+
+	await client.connect();
+
+	const checkResult = await client.query("SELECT id, name FROM department WHERE id = $1", [id]);
+
+	if (checkResult.rows.length === 0) {
+		console.error("Oddělení s tímto ID neexistuje.");
+		process.exit(1);
+	}
+
+	const departmentRow = checkResult.rows[0];
+	console.log(`Aktuální název: ${departmentRow.name}`);
+	const newName = promptSync("Zadej nový název oddělení: ");
+	await client.query("UPDATE department SET name = $1 WHERE id = $2", [newName, id]);
+	console.log(`Oddělení bylo aktualizováno. ID: ${id}, nový název: ${newName}.`);
+} finally {
+	await client.end();
+}
