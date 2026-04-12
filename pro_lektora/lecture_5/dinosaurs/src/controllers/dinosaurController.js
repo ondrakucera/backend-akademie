0f1b351d6739bc04f2f84c722d6cc9dbeeda8175
Ondra Kučera <ondrej.kucera@centrum.cz>
2026-04-12 12:08:03 +0200

    Lecture 5: dinosaurs: Migrate to Express 5
    This allows us to remove explicit try/catch blocks from controller
    methods.

--- pro_lektora/lecture_5/dinosaurs/src/controllers/dinosaurController.js
+++ pro_lektora/lecture_5/dinosaurs/src/controllers/dinosaurController.js
@@ -7,58 +7,38 @@ import {
 } from "../validation/dinosaurSchemas.js";
 
 export const dinosaurController = {
-	async list(request, response, next) {
-		try {
-			const dinosaurs = await dinosaurRepository.listDinosaurs();
-			response.json(dinosaurs);
-		} catch (error) {
-			next(error);
-		}
+	async list(request, response) {
+		const dinosaurs = await dinosaurRepository.listDinosaurs();
+		response.json(dinosaurs);
 	},
-	async create(request, response, next) {
-		try {
-			const dinosaur = dinosaurBodySchema.parse(request.body);
-			const id = await dinosaurRepository.createDinosaur(dinosaur);
-			response.status(201).json(id);
-		} catch (error) {
-			next(error);
-		}
+	async create(request, response) {
+		const dinosaur = dinosaurBodySchema.parse(request.body);
+		const id = await dinosaurRepository.createDinosaur(dinosaur);
+		response.status(201).json(id);
 	},
-	async getById(request, response, next) {
-		try {
-			const { id } = dinosaurIdParamSchema.parse(request.params);
-			const dinosaur = await dinosaurRepository.getDinosaurById(id);
-			if (!dinosaur) {
-				throw new DinosaurNotFoundError(id);
-			}
-			response.json(dinosaur);
-		} catch (error) {
-			next(error);
+	async getById(request, response) {
+		const { id } = dinosaurIdParamSchema.parse(request.params);
+		const dinosaur = await dinosaurRepository.getDinosaurById(id);
+		if (!dinosaur) {
+			throw new DinosaurNotFoundError(id);
 		}
+		response.json(dinosaur);
 	},
-	async updateById(request, response, next) {
-		try {
-			const { id } = dinosaurIdParamSchema.parse(request.params);
-			const dinosaur = dinosaurBodySchema.parse(request.body);
-			const updated = await dinosaurRepository.updateDinosaurById(id, dinosaur);
-			if (!updated) {
-				throw new DinosaurNotFoundError(id);
-			}
-			response.status(204).send();
-		} catch (error) {
-			next(error);
+	async updateById(request, response) {
+		const { id } = dinosaurIdParamSchema.parse(request.params);
+		const dinosaur = dinosaurBodySchema.parse(request.body);
+		const updated = await dinosaurRepository.updateDinosaurById(id, dinosaur);
+		if (!updated) {
+			throw new DinosaurNotFoundError(id);
 		}
+		response.status(204).send();
 	},
-	async deleteById(request, response, next) {
-		try {
-			const { id } = dinosaurIdParamSchema.parse(request.params);
-			const deleted = await dinosaurRepository.deleteDinosaurById(id);
-			if (!deleted) {
-				throw new DinosaurNotFoundError(id);
-			}
-			response.status(204).send();
-		} catch (error) {
-			next(error);
+	async deleteById(request, response) {
+		const { id } = dinosaurIdParamSchema.parse(request.params);
+		const deleted = await dinosaurRepository.deleteDinosaurById(id);
+		if (!deleted) {
+			throw new DinosaurNotFoundError(id);
 		}
+		response.status(204).send();
 	},
 };
