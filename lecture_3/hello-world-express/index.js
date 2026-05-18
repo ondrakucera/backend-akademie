480981c971bb776a1d2c63afe6f295f6194c9862
Ondra Kučera <ondrej.kucera@centrum.cz>
2026-05-18 18:53:46 +0200

    Temporary: Add lecture_3/hello-world-express

---
+++ lecture_3/hello-world-express/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/hello-world", (request, response) => {
+	response.send("Hello world!");
+});
+
+app.get("/hello-world-json", (request, response) => {
+	response.json({ message: "Hello world!", serverTime: new Date().toISOString() });
+});
+
+app.listen(3000);
